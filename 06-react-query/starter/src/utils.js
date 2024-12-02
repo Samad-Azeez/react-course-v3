b4eb740caa6e2056fe8a5a7541a99980ba403ff6
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-02 11:21:08 +0100

    create custom axios instance with a base URL for task API

--- 06-react-query/starter/src/utils.js
+++ 06-react-query/starter/src/utils.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// custom axios instance
+// Create a custom axios instance with a base URL
 const customFetch = axios.create({
   baseURL: 'http://localhost:5000/api/tasks',
 });
