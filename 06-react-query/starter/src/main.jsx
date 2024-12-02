9f20b63d8b1e49b0acdacff364c21efaad0369d6
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-02 14:38:41 +0100

    add react-query integration and configure QueryClientProvider in main.jsx

--- 06-react-query/starter/src/main.jsx
+++ 06-react-query/starter/src/main.jsx
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const queryClient = new QueryClient(); // Create a client instance to be used by the QueryClientProvider
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // Wrap the App component with the QueryClientProvider and pass the client instance as a prop
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
