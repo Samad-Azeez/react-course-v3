e1bb0b1d1e2a7465734f48b4357dd741e04494fd
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-03 11:47:07 +0100

    feat: Integrate Gallery, SearchForm, and ThemeToggle components into App

--- 07-unsplash-images/starter/src/App.jsx
+++ 07-unsplash-images/starter/src/App.jsx
@@ -1,4 +1,15 @@
+import Gallery from './Gallery';
+import SearchForm from './SearchForm';
+import ThemeToggle from './ThemeToggle';
+
 const App = () => {
-  return <h1>Unsplash Images Starter</h1>;
+  return (
+    <>
+      <h1>Unsplash Images Starter</h1>
+      <SearchForm />
+      <ThemeToggle />
+      <Gallery />
+    </>
+  );
 };
 export default App;
