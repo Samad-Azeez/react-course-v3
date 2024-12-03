b5ceef3a6d64d15f4ad26cb9726612d2aba093f6
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-03 09:31:17 +0100

    feat: Enhance task creation with success and error handling in Form component

--- 06-react-query/starter/src/Form.jsx
+++ 06-react-query/starter/src/Form.jsx
@@ -1,12 +1,26 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
 import customFetch from './utils';
+import { toast } from 'react-toastify';
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState('');
 
+  // Get the queryClient instance
+  const queryClient = useQueryClient();
+
   const { mutate: createTask, isLoading } = useMutation({
     mutationFn: (taskTitle) => customFetch.post('/', { title: taskTitle }),
+    onSuccess: () => {
+      // Invalidate the 'tasks' query to refetch the data
+      queryClient.invalidateQueries({ queryKey: ['tasks'] });
+      toast.success('Task added');
+      setNewItemName('');
+    },
+    onError: (error) => {
+      // Display an error toast message
+      toast.error(error.response.data.msg);
+    },
   });
 
   const handleSubmit = (e) => {
