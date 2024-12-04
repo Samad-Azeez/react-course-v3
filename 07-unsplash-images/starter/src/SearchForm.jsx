ca8f982629676129ac996f99f00c992c6401e34d
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-04 03:23:51 +0100

    feat: Implement search form with controlled input and submit handler

--- 07-unsplash-images/starter/src/SearchForm.jsx
+++ 07-unsplash-images/starter/src/SearchForm.jsx
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submitted');
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        name='search'
+        id=''
+        placeholder='cat'
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+    </form>
+  );
 };
 
 export default SearchForm;
