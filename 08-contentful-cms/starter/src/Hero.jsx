2babe2256d5edcac7e57143946bfe94f99b03af1
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-05 07:42:24 +0100

    feat: Update Hero component with new layout and image integration

--- 08-contentful-cms/starter/src/Hero.jsx
+++ 08-contentful-cms/starter/src/Hero.jsx
@@ -1,5 +1,24 @@
+import heroImg from './assets/hero.svg';
+
 const Hero = () => {
-  return <h2>Hero</h2>;
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <div className='hero-title'>
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, eum
+            excepturi? Adipisci officiis saepe placeat at doloribus tempore
+            tenetur nulla qui corrupti, voluptatem exercitationem accusantium
+            mollitia nemo nostrum, est vero?
+          </p>
+        </div>
+        <div className='image-container'>
+          <img src={heroImg} alt='woman and the browser' className='img' />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
