668f16cf006137dc94ead43d8fc6a8f6ecc92027
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-06 13:32:06 +0100

    feat: Implement custom hook useFetchProjects to manage projects data fetching

--- 08-contentful-cms/starter/src/fetchProjects.jsx
+++ 08-contentful-cms/starter/src/fetchProjects.jsx
@@ -1,24 +1,47 @@
+import { useState, useEffect } from 'react';
 import { createClient } from 'contentful';
 
+// init contentful client
 const client = createClient({
   space: 'f6gclpjtnunw',
   environment: 'master',
   accessToken: 'YWlefzKfzdIdCa5N8_QCzzcHFfrv3u0bw4m3Dz-LIUY',
 });
 
-client.getEntries({ content_type: 'projects' }).then((response) => {
-  console.log(response);
-});
+// custom hook to fetch and manage projects data
+const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
 
-// can be rewritten using async/await
+  const getData = async () => {
+    try {
+      // fetch projects from contentful
+      const response = await client.getEntries({ content_type: 'projects' });
+      // transform response data
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return { title, url, img, id };
+      });
 
-// const fetchProjects = async () => {
-//   try {
-//     const response = await client.getEntries({ content_type: 'projects' });
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
+      setProjects(projects);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
 
-// fetchProjects();
+  // fetch data on component mount
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { loading, projects };
+};
+
+// debug code (commented out)
+// client.getEntries({ content_type: 'projects' }).then((response) => {
+//   console.log(response);
+// });
