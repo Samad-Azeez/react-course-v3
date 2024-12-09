3240f8f498e27bfcbcb3396deaf37ecc2677c639
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-09 16:58:25 +0100

    feat: Add variables for name, age, and request status with initial values

--- 13-typescript-tutorial/src/tutorial.ts
+++ 13-typescript-tutorial/src/tutorial.ts
@@ -1,1 +1,8 @@
+let name: string = 'samad';
+name = name.toUpperCase();
 
+let age: number = 20;
+age = parseInt(age.toFixed(2));
+
+let requestStatus: 'pending' | 'success' | 'error' = 'pending';
+requestStatus = 'error';
