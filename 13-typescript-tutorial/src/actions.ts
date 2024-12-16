7519813e655365322ffe01577b6d800f1ac2de44
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-16 13:46:21 +0100

    testing

---
+++ 13-typescript-tutorial/src/actions.ts
@@ -0,0 +1,15 @@
+export function sayHello(name: string): void {
+  console.log(`Hello ${name}`);
+}
+
+export let person = 'samad';
+
+type Student = {
+  name: string;
+  age: number;
+};
+
+export const newStudent = {
+  name: 'john',
+  age: 20,
+};
