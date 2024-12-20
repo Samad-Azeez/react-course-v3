d0df5d4dc38a0475c75af182d99be119f8bb4a71
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-20 13:04:15 +0100

    feat: Update Component to accept props and children, enhancing usage in App component

--- 14-typescript-react-essentials/src/App.tsx
+++ 14-typescript-react-essentials/src/App.tsx
@@ -1,7 +1,11 @@
+import Component from './starter/02-props/index';
+
 function App() {
   return (
     <main>
-      <h2>React & Typescript</h2>
+      <Component name='samad' id={123}>
+        Hello this is a child
+      </Component>
     </main>
   );
 }
