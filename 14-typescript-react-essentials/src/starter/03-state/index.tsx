967248fb1dd4daa73c483f57e2fb4861489d110d
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-20 17:01:06 +0100

    feat: Add navigation links and update Component state management

--- 14-typescript-react-essentials/src/starter/03-state/index.tsx
+++ 14-typescript-react-essentials/src/starter/03-state/index.tsx
@@ -1,10 +1,42 @@
 import { useState } from 'react';
 
+type Link = { id: number; url: string; text: string };
+
+const navlinks = [
+  {
+    id: 1,
+    url: 'some url',
+    text: 'some text',
+  },
+  {
+    id: 2,
+    url: 'some url',
+    text: 'some text',
+  },
+  {
+    id: 3,
+    url: 'some url',
+    text: 'some text',
+  },
+];
+
 function Component() {
+  const [text, setText] = useState('Hello World');
+  const [list, setList] = useState<string[]>([]);
+  const [links, setLinks] = useState<Link[]>(navlinks);
+
   return (
     <div>
       <h2 className='mb-1'>React & Typescript</h2>
-      <button className='btn btn-center'>click me</button>
+      <button
+        className='btn btn-center'
+        onClick={() => {
+          setText('typeScript');
+          setLinks([...links, { id: 4, url: 'some url', text: 'some text' }]);
+        }}
+      >
+        click me
+      </button>
     </div>
   );
 }
