3de958eecf7a67e01ecbce79a03fd065c5f086b5
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-20 19:56:38 +0100

    feat: Update Component to handle form inputs and events for better user interaction

--- 14-typescript-react-essentials/src/starter/04-events/index.tsx
+++ 14-typescript-react-essentials/src/starter/04-events/index.tsx
@@ -1,9 +1,47 @@
+import { useState } from 'react';
+
 function Component() {
+  const [text, setText] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    console.log(Object.fromEntries(formData));
+  };
+
   return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Events</h2>
-    </div>
+    <section>
+      <h2>events example</h2>
+      <form className='form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          className='form-input mb-1'
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          name='text'
+        />
+        {/* second input to handle email value */}
+        <input
+          type='email'
+          className='form-input mb-1'
+          value={email}
+          onChange={handleChange}
+          name='email'
+        />
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+    </section>
   );
 }
 export default Component;
